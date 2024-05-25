0e64f8a602b2c184cf17e1a009157e1f8e2ff66b
unknown <yaramuhammad762@gmail.com>
2024-05-25 20:31:28 +0300

    admin

---
+++ backend/app/Http/Controllers/AdminAuthenticationController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminAuthenticationController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('guest:admin')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.admin-login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::guard('admin')->attempt($credentials, $request->filled('remember'))) {
+            $request->session()->regenerate();
+
+            return redirect()->route('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,12 +1,21 @@
 <?php
 
+use App\Http\Controllers\AdminAuthenticationController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+
 
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
     ->middleware(['throttle:6,1'])
     ->name('verification.verify');
+
+Route::view('/','dashboard')->middleware('auth:admin');
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/login', [AdminAuthenticationController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('/login', [AdminAuthenticationController::class, 'login']);
+    Route::post('/logout', [AdminAuthenticationController::class, 'logout'])->name('admin.logout');
+});
