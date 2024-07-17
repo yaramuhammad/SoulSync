b8ad0428fa1d6887ba67d8951e3c75a2b7d43563
Yara <yaramuhammad762@gmail.com>
2024-07-17 19:22:57 +0300

    guest admin middleware

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
             'admin' => \App\Http\Middleware\RedirectIfNotAdmin::class,
-
+            'guest:admin' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         ]);
 
         //
